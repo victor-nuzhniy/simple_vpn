aaade425900bdac09e89b9e62116aef7d097a6bd
victro-nuzhniy <nuzhniyva@gmail.com>
2023-11-14 23:42:16 +0200

    dev added RegisterView in views and added its url in vpn urls

--- vpn/urls.py
+++ vpn/urls.py
@@ -1,1 +1,10 @@
 """Urls for vpn app."""
+from django.urls import path
+
+from vpn.views import RegisterView
+
+app_name = "vpn"
+
+urlpatterns = [
+    path("sign_up/", RegisterView.as_view(), name="sign_up"),
+]
--- vpn/views.py
+++ vpn/views.py
@@ -1,1 +1,22 @@
 """Class and function views for vpn app."""
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
+from django.urls import reverse_lazy
+from django.views.generic import FormView
+
+
+class RegisterView(FormView):
+    """View for user registration."""
+
+    form_class = UserCreationForm
+    template_name = "vpn/user_creation_form.html"
+    extra_context = {"title": "Sign up"}
+    success_url = reverse_lazy("vpn:sign_up")
+
+    def form_valid(self, form: UserCreationForm) -> HttpResponseRedirect:
+        """Create new user and login."""
+        user: User = form.save()
+        login(self.request, user)
+        return super().form_valid(form)
