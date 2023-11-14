f21d1495b366d2c2a01fb87b94a22caa757b45a9
victro-nuzhniy <nuzhniyva@gmail.com>
2023-11-14 23:12:13 +0200

    dev created vpn urls and added it to config urls

--- config/urls.py
+++ config/urls.py
@@ -16,12 +16,13 @@ Including another URLconf
 """
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 from config import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", include("vpn.urls")),
 ]
 
 if settings.DEBUG:
---
+++ vpn/urls.py
@@ -0,0 +1,1 @@
+"""Urls for vpn app."""
