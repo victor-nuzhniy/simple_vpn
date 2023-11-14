6effcde8ea4505f96fe34118afde9e20e30c7263
victro-nuzhniy <nuzhniyva@gmail.com>
2023-11-14 22:10:38 +0200

    dev created vpn models

--- vpn/models.py
+++ vpn/models.py
@@ -1,1 +1,48 @@
 """Vpn app models."""
+from __future__ import annotations
+
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class PersonalSite(models.Model):
+    """Model for user site."""
+
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Site owner")
+    name = models.SlugField(max_length=100, verbose_name="Site name")
+    slug = models.CharField(max_length=100, unique=True, verbose_name="Site slug")
+
+    def __str__(self) -> str:
+        """Represent model."""
+        return f"{self.owner} {self.name}"
+
+
+class Page(models.Model):
+    """Model for site page."""
+
+    name = models.CharField(max_length=100, verbose_name="Page name")
+    slug = models.SlugField(max_length=100, verbose_name="Page slug")
+    personal_site = models.ForeignKey(
+        PersonalSite, models.CASCADE, verbose_name="Personal site"
+    )
+    sended = models.FloatField(verbose_name="Sended data")
+    loaded = models.FloatField(verbose_name="Loaded data")
+    links = models.ManyToManyField(
+        "self", through="PageLinks", verbose_name="Links on page"
+    )
+
+    def __str__(self) -> str:
+        """Represent model."""
+        return f"{self.name}"
+
+
+class PageLinks(models.Model):
+    """Model for 'many-to-many' relationships to self Page model."""
+
+    from_page = models.ForeignKey(Page, models.CASCADE, verbose_name="Page with link")
+    to_page = models.ForeignKey(Page, models.CASCADE, verbose_name="Link to page")
+    quantity = models.IntegerField(verbose_name="Links was used")
+
+    def __str__(self) -> str:
+        """Represent model."""
+        return f"From {self.from_page} to {self.to_page}"
