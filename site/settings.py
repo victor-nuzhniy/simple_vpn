bb820b77f943055ef303e3ec59d0daae052fca06
victro-nuzhniy <nuzhniyva@gmail.com>
2023-11-14 22:44:27 +0200

    created site and vpn_site projects settings

---
+++ site/settings.py
@@ -0,0 +1,7 @@
+"""Settings for site project."""
+
+from config.settings import *
+
+SITE_ID = 1
+
+WSGI_APPLICATION = "site.wsgi.application"
---
+++ vpn_site/settings.py
@@ -0,0 +1,7 @@
+"""Settings for vpn_site project."""
+
+from config.settings import *
+
+SITE_ID = 2
+
+WSGI_APPLICATION = "vpn_site.wsgi.application"
