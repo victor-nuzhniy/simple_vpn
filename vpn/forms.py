168d7bdc9ba1cb64f5d54dc79d06a3e285d6774b
victro-nuzhniy <nuzhniyva@gmail.com>
2023-11-15 16:12:10 +0200

    dev deleted limit_choice_to attr in links field Page model

--- vpn/forms.py
+++ vpn/forms.py
@@ -1,14 +1,36 @@
 """Forms for vpn app."""
 from django import forms
 
-from vpn.models import PersonalSite
+from vpn.models import Page, PersonalSite
 
 
-class PersonalSiteForm(forms.ModelForm):
-    """Form for PersonalSite model."""
+class PersonalSiteCreateForm(forms.ModelForm):
+    """Form for PersonalSite model instance creation."""
 
     class Meta:
-        """Class Meta for PersonalSiteForm."""
+        """Class Meta for PersonalSiteCreateForm."""
 
         model = PersonalSite
         fields = ("name", "slug")
+
+
+class PageCreateForm(forms.ModelForm):
+    """Form for Page model instance creation."""
+
+    class Meta:
+        """Class Meta for PageCreateForm."""
+
+        model = Page
+        fields = "__all__"
+
+    def __init__(self, *args, **kwargs) -> None:
+        """Customize personal_site and links fields querysets."""
+        owner = kwargs.pop("owner")
+        super(PageCreateForm, self).__init__(*args, **kwargs)
+        if owner:
+            self.fields["personal_site"].queryset = PersonalSite.objects.filter(
+                owner=owner
+            )
+            self.fields["links"].queryset = Page.objects.filter(
+                personal_site__owner=owner
+            )
