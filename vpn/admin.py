f95ef77703e7ed7e2fb274f382603b85d41d2141
victro-nuzhniy <nuzhniyva@gmail.com>
2023-11-16 22:01:43 +0200

    dev added inline form to PageAdmin in admin site

--- vpn/admin.py
+++ vpn/admin.py
@@ -1,8 +1,30 @@
 """Admin site configuration for vpn app."""
+from django import forms
 from django.contrib import admin
 from django.db.models import Q
 
 from vpn.models import Page, PageLinks, PersonalSite
+
+
+class PageLinksInlineForm(forms.ModelForm):
+    """Rewrite PageLinksInlineForm link field queryset."""
+
+    def __init__(self, *args, **kwargs):
+        """Rewrite init method PageLinksInlineForm."""
+        super().__init__(*args, **kwargs)
+        if "instance" in kwargs:
+            self.base_fields["link"].queryset = Page.objects.filter(
+                personal_site__owner=self.instance.page.personal_site.owner
+            ).filter(~Q(id__in=[self.instance.page.id]))
+
+
+class PageLinksInline(admin.StackedInline):
+    """Inline PageLinks admin site configuration."""
+
+    model = PageLinks
+    form = PageLinksInlineForm
+    extra = 1
+    fk_name = "page"
 
 
 class PersonalSiteAdmin(admin.ModelAdmin):
@@ -19,6 +41,7 @@ class PageAdmin(admin.ModelAdmin):
     list_display = ("id", "name", "slug", "personal_site", "sended", "loaded")
     list_display_links = ("id", "name", "personal_site", "sended", "loaded")
     list_filter = ("personal_site",)
+    inlines = [PageLinksInline]
 
     def get_form(self, request, obj=None, change=False, **kwargs):
         """Change personal_site form field queryset."""
