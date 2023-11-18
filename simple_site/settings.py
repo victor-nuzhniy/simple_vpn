02a0575e3342f4d479d19c89c963839082ba9f89
victro-nuzhniy <nuzhniyva@gmail.com>
2023-11-18 19:21:58 +0200

    dev rename site package to simple_site (settings.py), added docker-compose.yml, docker-entrypoint.sh, Dockerfile

---
+++ simple_site/settings.py
@@ -0,0 +1,7 @@
+"""Settings for simple_site project."""
+
+from config.settings import *
+
+SITE_ID = 1
+
+WSGI_APPLICATION = "simple_site.wsgi.application"
--- site/settings.py
+++
@@ -1,7 +0,0 @@
-"""Settings for site project."""
-
-from config.settings import *
-
-SITE_ID = 1
-
-WSGI_APPLICATION = "site.wsgi.application"
